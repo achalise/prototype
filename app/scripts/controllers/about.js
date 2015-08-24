d58122fd7cc7fa1389fb6a8f4a674b06cb62af89
Arun Chalise <chalise.arun@gmail.com>
2015-08-24 14:32:39 +1000

    Project set up

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name payerPortalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the payerPortalApp
+ */
+angular.module('payerPortalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name payerPortalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the payerPortalApp
+ */
+angular.module('payerPortalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
